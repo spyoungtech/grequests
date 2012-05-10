90bc5b6d5c4aa35b83525035c5d4d17bea0b0d43
Kenneth Reitz <me@kennethreitz.com>
2012-05-10 17:22:38 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+GRequests allows you to use Requests with Gevent to make asyncronous HTTP
+Requests easily.
+
+Usage
+-----
+
+Usage is simple::
+
+    import grequests
+
+    urls = [
+        'http://www.heroku.com',
+        'http://tablib.org',
+        'http://httpbin.org',
+        'http://python-requests.org',
+        'http://kennethreitz.com'
+    ]
+
+Create a set of unsent Requests::
+
+    >>> rs = (grequests.get(u) for u in urls)
+
+Send them all at the same time::
+
+    >>> grequests.map(rs)
+    [<Response [200]>, <Response [200]>, <Response [200]>, <Response [200]>, <Response [200]>]
+
+"""
+
+from setuptools import setup
+
+setup(
+    name='grequests',
+    version='0.1.0',
+    url='https://github.com/kennethreitz/grequests',
+    license='BSD',
+    author='Kenneth Reitz',
+    author_email='me@kennethreitz.com',
+    description='Requests + Gevent',
+    long_description=__doc__,
+    py_modules=['grequests'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
