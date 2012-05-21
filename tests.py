21c95dcb5bd55787ddfc2b7690db64f639911d9d
kracekumar <kracethekingmaker@gmail.com>
2012-05-21 21:49:39 +0530

    made suggest changes

--- tests.py
+++ tests.py
@@ -1,24 +1,23 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from grequests import get, map, imap
 from nose.tools import ok_
 
+########### Constants ############
+urls = [
+        'http://github.com',
+        'http://www.google.com', 
+        'http://www.psf.org'
+        ]
+############# tests ##############
 def test_get():
-    urls = [
-            'http://github.com',
-            'http://www.google.com', 
-            'http://www.psf.org'
-            ]
     to_fetch = (get(url) for url in urls)
     map(to_fetch)
     for fetched in to_fetch:
         ok_(fetched.ok, True)
 
 def test_imap_with_size():
-    urls = [
-            'http://github.com',
-            'http://www.google.com', 
-            'http://www.psf.org',
-            'http://www.twitter.com',
-            ]
     to_fetch = (get(url) for url in urls)
     imap(to_fetch, size = len(urls) - 1)
     for fetching in to_fetch:
