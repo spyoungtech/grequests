1a5318439176f40911399136ef4797d9cba2d587
kracekumar <kracethekingmaker@gmail.com>
2012-05-21 21:51:50 +0530

    made urls to be imported under global

--- tests.py
+++ tests.py
@@ -12,12 +12,14 @@ urls = [
         ]
 ############# tests ##############
 def test_get():
+    global urls
     to_fetch = (get(url) for url in urls)
     map(to_fetch)
     for fetched in to_fetch:
         ok_(fetched.ok, True)
 
 def test_imap_with_size():
+    global urls
     to_fetch = (get(url) for url in urls)
     imap(to_fetch, size = len(urls) - 1)
     for fetching in to_fetch:
