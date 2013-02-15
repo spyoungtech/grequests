14aedefbe7139b8ab096575dc7f5453ac64adb6c
Phil Howell <phil@quae.co.uk>
2013-02-15 18:20:40 +0000

    requests 1.x.x compatability

--- erequests.py
+++ erequests.py
@@ -9,14 +9,15 @@ by eventlet. All API methods return a ``Request`` instance (as opposed to
 ``Response``). A list of requests can be sent with ``map()``.
 """
 
+from functools import partial
+
 import eventlet
 from eventlet.greenpool import GreenPool
 
 # Monkey-patch.
 eventlet.monkey_patch(os=True, socket=True, time=True)
 
-from requests import api
-
+from requests import Session
 
 __all__ = (
     'map', 'imap',
@@ -24,50 +25,83 @@ __all__ = (
 )
 
 
-def patched(f):
-    """Patches a given API function to not send."""
+class AsyncRequest(object):
+    """ Asynchronous request.
 
-    def wrapped(*args, **kwargs):
-        config = kwargs.get('config', {})
-        config['safe_mode'] = True
-        kwargs.update(dict(return_response=False, prefetch=True, config=config))
-        return f(*args, **kwargs)
-    return wrapped
+    Accept same parameters as ``Session.request`` and some additional:
+
+    :param session: Session which will do request
+    :param callback: Callback called on response. Same as passing ``hooks={'response': callback}``
+    """
+    def __init__(self, method, url, **kwargs):
+        #: Request method
+        self.method = method
+        #: URL to request
+        self.url = url
+        #: Associated ``Session``
+        self.session = kwargs.pop('session', None)
+        if self.session is None:
+            self.session = Session()
+
+        callback = kwargs.pop('callback', None)
+        if callback:
+            kwargs['hooks'] = {'response': callback}
+
+        #: The rest arguments for ``Session.request``
+        self.kwargs = kwargs
+        #: Resulting ``Response``
+        self.response = None
+
+    def send(self, **kwargs):
+        """
+        Prepares request based on parameter passed to constructor and optional ``kwargs```.
+        Then sends request and saves response to :attr:`response`
+
+        :returns: ``Response``
+        """
+        merged_kwargs = {}
+        merged_kwargs.update(self.kwargs)
+        merged_kwargs.update(kwargs)
+        self.response = self.session.request(self.method, self.url, **merged_kwargs)
+        return self.response
 
 
-def send(r, pool=None, prefetch=False):
+def send(r, pool=None, stream=False):
     """Sends the request object using the specified pool. If a pool isn't
     specified this method blocks. Pools are useful because you can specify size
     and can hence limit concurrency."""
-
     if pool is not None:
-        return pool.spawn(r.send, prefetch=prefetch)
-    return eventlet.spawn(r.send, prefetch=prefetch)
+        return pool.spawn(r.send, stream=stream)
+    return eventlet.spawn(r.send, stream=stream)
 
 
-# Patched requests.api functions.
-get = patched(api.get)
-options = patched(api.options)
-head = patched(api.head)
-post = patched(api.post)
-put = patched(api.put)
-patch = patched(api.patch)
-delete = patched(api.delete)
-request = patched(api.request)
+# Shortcuts for creating AsyncRequest with appropriate HTTP method
+get = partial(AsyncRequest, 'GET')
+options = partial(AsyncRequest, 'OPTIONS')
+head = partial(AsyncRequest, 'HEAD')
+post = partial(AsyncRequest, 'POST')
+put = partial(AsyncRequest, 'PUT')
+patch = partial(AsyncRequest, 'PATCH')
+delete = partial(AsyncRequest, 'DELETE')
 
 
-def map(requests, prefetch=True, size=None):
+# synonym
+def request(method, url, **kwargs):
+    return AsyncRequest(method, url, **kwargs)
+
+
+def map(requests, stream=False, size=None):
     """Concurrently converts a list of Requests to Responses.
 
     :param requests: a collection of Request objects.
-    :param prefetch: If False, the content will not be downloaded immediately.
+    :param stream: If True, the content will not be downloaded immediately.
     :param size: Specifies the number of requests to make at a time. If None, no throttling occurs.
     """
 
     requests = list(requests)
 
     pool = GreenPool(size) if size else None
-    jobs = [send(r, pool, prefetch=prefetch) for r in requests]
+    jobs = [send(r, pool, stream=stream) for r in requests]
     if pool is not None:
         pool.waitall()
     else:
@@ -76,21 +110,22 @@ def map(requests, prefetch=True, size=None):
     return [r.response for r in requests]
 
 
-def imap(requests, prefetch=True, size=2):
+def imap(requests, stream=False, size=2):
     """Concurrently converts a generator object of Requests to
     a generator of Responses.
 
     :param requests: a generator of Request objects.
-    :param prefetch: If False, the content will not be downloaded immediately.
+    :param stream: If True, the content will not be downloaded immediately.
     :param size: Specifies the number of requests to make at a time. default is 2
     """
 
-    def send(r):
-        r.send(prefetch)
-        return r.response
-
     pool = GreenPool(size)
+
+    def send(r):
+        return r.send(stream=stream)
+
     for r in pool.imap(send, requests):
         yield r
+
     pool.waitall()
 
