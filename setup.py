12e278abd546c2e61583882c3463361a2e984f3b
Saúl Ibarra Corretgé <saghul@gmail.com>
2013-04-25 09:00:51 +0200

    Raised version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name                 = 'erequests',
-    version              = '0.2.0',
+    version              = '0.2.1',
     url                  = 'https://github.com/saghul/erequests',
     license              = 'BSD',
     author               = 'Kenneth Reitz',
