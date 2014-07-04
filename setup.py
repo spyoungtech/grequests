6d622f477e2c80a9f0f4df0b5f6613eac94c1c97
Ryan T. Dean <ryand@netflix.com>
2014-07-03 18:10:12 -0700

    bump version number to 0.3.0

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ from setuptools import setup
 
 setup(
     name='grequests',
-    version='0.1.0',
+    version='0.3.0',
     url='https://github.com/kennethreitz/grequests',
     license='BSD',
     author='Kenneth Reitz',
