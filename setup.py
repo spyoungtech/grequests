fa7a4d7a766237a6643d7a5cabd39feffb729c37
Kenneth Reitz <me@kennethreitz.org>
2016-01-31 03:01:08 -0500

    v0.3.0

--- setup.py
+++ setup.py
@@ -6,26 +6,59 @@ Requests easily.
 Usage
 -----
 
-Usage is simple::
+Usage is simple:
+
+.. code-block:: python
 
     import grequests
 
     urls = [
         'http://www.heroku.com',
-        'http://tablib.org',
+        'http://python-tablib.org',
         'http://httpbin.org',
         'http://python-requests.org',
+        'http://fakedomain/',
         'http://kennethreitz.com'
     ]
 
-Create a set of unsent Requests::
+Create a set of unsent Requests:
+
+.. code-block:: python
 
     >>> rs = (grequests.get(u) for u in urls)
 
-Send them all at the same time::
+Send them all at the same time:
+
+.. code-block:: python
 
     >>> grequests.map(rs)
-    [<Response [200]>, <Response [200]>, <Response [200]>, <Response [200]>, <Response [200]>]
+    [<Response [200]>, <Response [200]>, <Response [200]>, <Response [200]>, None, <Response [200]>]
+
+Optionally, in the event of a timeout or any other exception during the connection of
+the request, you can add an exception handler that will be called with the request and
+exception inside the main thread:
+
+.. code-block:: python
+
+    >>> def exception_handler(request, exception):
+    ...    print "Request failed"
+
+    >>> reqs = [
+    ...    grequests.get('http://httpbin.org/delay/1', timeout=0.001),
+    ...    grequests.get('http://fakedomain/'),
+    ...    grequests.get('http://httpbin.org/status/500')]
+    >>> grequests.map(reqs, exception_handler=exception_handler)
+    Request failed
+    Request failed
+    [None, None, <Response [500]>]
+
+
+Installation
+------------
+
+Installation is easy with pip::
+
+    $ pip install grequests
 
 """
 
@@ -33,7 +66,7 @@ from setuptools import setup
 
 setup(
     name='grequests',
-    version='0.3.1',
+    version='0.3.0',
     url='https://github.com/kennethreitz/grequests',
     license='BSD',
     author='Kenneth Reitz',
